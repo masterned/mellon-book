2dd38867e246f2571626c07f9da8513817709a24
Spencer Dent <mr.spencerdent@gmail.com>
2025-02-08 19:10:53 -0500

    feat: create `SwapResult` trait for `Result`s
    This swaps the value from `Ok` to `Err` and vice versa.

---
+++ src/utils/mod.rs
@@ -0,0 +1,26 @@
+pub trait SwapResult<T, E> {
+    fn swap(self) -> Result<E, T>;
+}
+
+impl<T, E> SwapResult<T, E> for Result<T, E> {
+    fn swap(self) -> Result<E, T> {
+        match self {
+            Ok(t) => Err(t),
+            Err(e) => Ok(e),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _swaps_result_ok_and_err() {
+        let result: Result<usize, ()> = Ok(42);
+        assert_eq!(result.swap(), Err(42));
+
+        let result: Result<(), usize> = Err(42);
+        assert_eq!(result.swap(), Ok(42));
+    }
+}
