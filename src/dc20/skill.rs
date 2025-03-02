e4fdaf9c97bb34a2dc6f2695b3e3b4526ee82062
Spencer Dent <mr.spencerdent@gmail.com>
2025-03-02 18:31:33 -0500

    fix: properly apply combat mastery to save

--- src/dc20/skill.rs
+++ src/dc20/skill.rs
@@ -22,9 +22,13 @@ pub struct Stat {
 impl Stat {
     #[must_use]
     pub fn calc_save(&self, level: Level) -> isize {
-        self.score
-            .checked_add_unsigned(level.calc_combat_mastery())
-            .unwrap()
+        if self.save_proficiency {
+            self.score
+                .checked_add_unsigned(level.calc_combat_mastery())
+                .unwrap()
+        } else {
+            self.score
+        }
     }
 }
 
@@ -54,6 +58,73 @@ impl Skill {
             mastery as isize + stat.score
         } else {
             stat.score
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod stat {
+        use super::*;
+
+        #[test]
+        fn _stat_without_save_mastery_should_have_save_of_stat_score() {
+            let stat = Stat {
+                name: "Test Stat".into(),
+                score: 3,
+                save_proficiency: false,
+                skills: vec![],
+            };
+
+            assert_eq!(stat.calc_save(Level::default()), stat.score);
+        }
+
+        #[test]
+        fn _stat_with_save_mastery_should_add_combat_mastery_to_save() {
+            let stat = Stat {
+                name: "Test Stat".into(),
+                score: 3,
+                save_proficiency: true,
+                skills: vec![],
+            };
+            let level = Level::default();
+            let combat_mastery = level.calc_combat_mastery() as isize;
+
+            assert_eq!(stat.calc_save(level), stat.score + combat_mastery)
+        }
+    }
+
+    mod skill {
+        use super::*;
+
+        #[test]
+        fn _skill_without_mastery_should_have_same_score_as_stat() {
+            let skill = Skill::new("Test Skill");
+            let stat = Stat {
+                name: "Intelligence".into(),
+                score: 3,
+                save_proficiency: true,
+                skills: vec![skill.clone()],
+            };
+
+            assert_eq!(skill.calc_score(&stat), stat.score);
+        }
+
+        #[test]
+        fn _skill_with_mastery_should_add_mastery_value_to_score() {
+            let mut skill = Skill::new("Test Skill");
+            skill.set_mastery(Mastery::Novice);
+
+            let stat = Stat {
+                name: "Intelligence".into(),
+                score: 3,
+                save_proficiency: true,
+                skills: vec![skill.clone()],
+            };
+
+            assert_eq!(skill.calc_score(&stat), stat.score + 2);
         }
     }
 }
