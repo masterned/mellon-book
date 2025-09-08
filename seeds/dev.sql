c7ea8a43a0273064e7f93aedbecaf2233d71b884
Spencer Dent <mr.spencerdent@gmail.com>
2025-09-08 15:13:06 -0400

    fix: insert blobs into uuid instead of text

--- seeds/dev.sql
+++ seeds/dev.sql
@@ -1,18 +1,18 @@
 INSERT INTO `players` (`name`, `uuid`)
-    VALUES ("Spencer Dent", "01991836-ac9f-7589-8eff-73915fd87018")
+    VALUES ("Spencer Dent", X'01991836ac9f75898eff73915fd87018')
 ;
 
 INSERT INTO `characters` (`name`, `creator_id`, `uuid`)
-    VALUES ("Cygnus", 1, "01991836-da19-7229-8430-f8ad85a67ee0")
+    VALUES ("Cygnus", 1, X'01991836da1972298430f8ad85a67ee0')
 ;
 
 INSERT INTO `ancestries` (`uuid`, `name`)
-    VALUES ("01991828-2492-7164-b25d-368464947b6a", "Human")
+    VALUES (X'0199182824927164b25d368464947b6a', "Human")
 ;
 
 INSERT INTO `ancestry_traits` (`uuid`, `name`, `description`, `cost`)
     VALUES (
-        "01991828-aa3c-7fa9-a24b-c2afacaa349d",
+        X'01991828aa3c7fa9a24bc2afacaa349d',
         "Attribute Increase",
         "Choose an Attribute. The chosen Attribute increases by 1 (up to the Attribute Limit).",
         2
