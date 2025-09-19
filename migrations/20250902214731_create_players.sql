9d469719312056c9ef4e64cab752e118201994de
Spencer Dent <mr.spencerdent@gmail.com>
2025-09-19 08:10:47 -0400

    chore: remove unused migrations directory
    Since I've been using atlas to manage the schema declaratively, I no
    longer need a migrations directory to store each iteration. If I want to
    look through historical structures, I can just checkout previous
    commits.

--- migrations/20250902214731_create_players.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `players` (
-    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `uuid` BLOB NOT NULL,
-    `name` TEXT NOT NULL
-);
-
-CREATE UNIQUE INDEX `players_uuid_uniq`
-    ON `players` (`uuid`);
